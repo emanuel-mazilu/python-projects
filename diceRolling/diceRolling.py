10ca67ace7832441fdb1c4cbb07d7d8d94ebf27d
Emanuel Mazilu <emanuel-mazilu@outlook.com>
2020-04-18 08:43:14 +0300

    initial commit

---
+++ diceRolling/diceRolling.py
@@ -0,0 +1,27 @@
+import random
+
+print("Rolling dice simulator !!!")
+
+finish = "y"
+while finish == "y":
+
+    sum = 0
+    dices = input("How many dices do you want to roll?: ")
+
+    # Check if numeric user input
+    if dices.isdigit():
+        # User input is str, cast to int
+        dices = int(dices)   
+        # Generate random numbers for all dices        
+        for i in range(dices):
+            dice = random.randint(1,6)
+            print(dice, end=" ")
+            # Make a sum of all dices
+            sum += dice
+
+        print() # New Line
+        print(f"All toghether, you rolled: {sum}")
+        print(f"The avrage of the rolls is {sum/dices}")
+        finish = input("Do you want to play again? (y/n)")
+    else:
+        print("I need a number")
---
+++ numberGuessing/numberGuessing.py
@@ -0,0 +1,18 @@
+import random
+
+numberToGuess = random.randint(1, 100)
+
+while True:
+    guess = input("Guess the number: ")
+    if guess.isdigit():
+        guess = int(guess)
+        if guess == numberToGuess:
+            print("Correct")
+            # if true exit loop
+            break
+        elif guess < numberToGuess:
+            print('Guess higher')
+        else:
+            print("Guess lower")
+    else:
+        print("Must be a number (1-100)")
